cb4dffaaffd5e55e2a990eef0bb948624b77576a
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2022-12-17 12:17:30 +0000

    feat: :sparkles: Added lots of basic foundational things like synapses and genes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::world::World;
 
 pub mod creatures;
+pub mod sensors;
 pub mod world;
 
 #[cfg(test)]
@@ -8,8 +9,22 @@ mod tests;
 
 fn main() {
     println!("Hello, world!");
-
+    // Initial world generation
     let world = World::new(128, 128, 10);
 
-    println!("{}", world);
+    // One tick
+}
+
+fn tick(world: &World) -> World {
+    let mut new_world = World::blank(world.x_len, world.y_len);
+
+    for row in &world.locs {
+        for tile in row {
+            if tile.is_some() {
+                let creature = tile.as_ref().unwrap();
+            }
+        }
+    }
+
+    todo!();
 }
